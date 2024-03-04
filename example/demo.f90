ebef25d849b43cf93e0721de27521c626bbb4b28
Ed Hone <edwardhone@gmail.com>
2024-03-04 18:53:28 +0000

    Error level now errors

--- example/demo.f90
+++ example/demo.f90
@@ -1,12 +1,13 @@
 program demo
 
-use oolong, only: logger_type, COLOUR_GREEN, LEVEL_ALWAYS, LEVEL_INFO, LEVEL_TRACE, LEVEL_WARNING
+use oolong, only: logger_type, COLOUR_GREEN, LEVEL_ALWAYS, LEVEL_INFO, &
+                  LEVEL_TRACE, LEVEL_WARNING, LEVEL_ERROR
 
 implicit none
 
 type(logger_type) :: log, log1, log2
 
-log = logger_type(LEVEL_INFO)
+log = logger_type(LEVEL_INFO, stop_level=LEVEL_WARNING)
 log1 = logger_type(LEVEL_INFO, id = "Foo")
 log2 = logger_type(LEVEL_TRACE, id = "Bar", colour=COLOUR_GREEN)
 
@@ -17,5 +18,6 @@ call log2%event("Hello from logger 2", LEVEL_INFO)
 call log2%event("Testing trace level from logger 2", LEVEL_TRACE)
 
 call log%event("Test warning", LEVEL_WARNING)
+call log%event("Test error", LEVEL_ERROR)
 
 end program demo
--- src/logger/logger_mod.f90
+++ src/logger/logger_mod.f90
@@ -4,7 +4,8 @@ module logger_mod
     use, intrinsic :: iso_fortran_env, only : output_unit, error_unit
 
     use colour_mod, only: change_colour, COLOUR_WHITE, COLOUR_GREY
-    use levels_mod, only: logger_level_is_valid, get_log_level_str, level_is_err
+    use levels_mod, only: logger_level_is_valid, get_log_level_str, &
+                          level_is_err, LEVEL_ERROR
 
     implicit none
 
@@ -17,9 +18,11 @@ module logger_mod
         integer           :: output_stream
         integer           :: colour_tag = COLOUR_WHITE
         logical           :: outputs_to_term
+        integer           :: stop_level = LEVEL_ERROR
     contains
         procedure, public :: event
         procedure         :: format_info
+        procedure         :: all_stop
     end type
 
     interface logger_type
@@ -29,7 +32,7 @@ module logger_mod
 contains
 
     !> Constructor for the logger object
-    function logger_constructor(level, id, colour) result(self)
+    function logger_constructor(level, id, colour, stop_level) result(self)
 
         implicit none
 
@@ -37,6 +40,7 @@ contains
         integer,                    intent(in) :: level
         character(len=*), optional, intent(in) :: id
         integer,          optional, intent(in) :: colour
+        integer,          optional, intent(in) :: stop_level
 
         if (logger_level_is_valid(level)) then
             self%log_level = level
@@ -51,6 +55,17 @@ contains
 
         if (present(id)) self%id = trim(id)
         if (present(colour)) self%colour_tag = colour
+
+        if (present(stop_level)) then
+            if (logger_level_is_valid(stop_level) .and. &
+                    level_is_err(stop_level)) then
+                self%stop_level = stop_level
+            else
+                write(error_unit, '(A)') &
+                    "ERROR - oolong__logger_mod_62: Invalid choice for stop_level"
+                stop 1
+            end if
+        end if
 
     end function logger_constructor
 
@@ -75,7 +90,7 @@ contains
         if (self%log_level > level) return
 
         dt_str = datetime_string()
-        info_str = self%format_info()
+        info_str = self%format_info(level)
 
         if (self%outputs_to_term) then
             fmt_dt_str = change_colour(dt_str, COLOUR_GREY)
@@ -92,22 +107,26 @@ contains
         write ( log_out, '("[", A,"]" A, ": ", A)' ) &
             trim(fmt_dt_str), trim(info_str), trim(message)
 
+        if (level >= self%stop_level) call self%all_stop()
+
     end subroutine event
 
     !> Formats information about this logger object ready for output
     !!
+    !> level  Log level of the message
     !> return Character array of length 32
-    function format_info(self) result(result_string)
+    function format_info(self, level) result(result_string)
 
         implicit none
 
         class(logger_type), intent(inout) :: self
+        integer,            intent(in)    :: level
 
         character(len=32) :: result_string, fmt_tag_str
 
         if (trim(self%id) == "None") then
             write( result_string, '(" ", A)' ) &
-                trim(get_log_level_str(self%log_level))
+                trim(get_log_level_str(level))
         else
             if (self%outputs_to_term) then
                 fmt_tag_str = change_colour(trim(self%id), self%colour_tag)
@@ -115,7 +134,7 @@ contains
                 fmt_tag_str = trim(self%id)
             end if
             write( result_string, '("[", A,"] ", A)' ) &
-                trim(fmt_tag_str), trim(get_log_level_str(self%log_level))
+                trim(fmt_tag_str), trim(get_log_level_str(level))
         end if
 
     end function format_info
@@ -140,5 +159,15 @@ contains
                     datetime_values(8)
 
     end function datetime_string
+
+    subroutine all_stop(self)
+
+        implicit none
+
+        class(logger_type), intent(inout) :: self
+
+        stop 1
+
+    end subroutine all_stop
 
 end module logger_mod

7c6c46ae5b7ace3a75c1c2b17291384d38f153b4
Ed Hone <edwardhone@gmail.com>
2024-03-04 15:11:01 +0000

    Colour change functionality working - untested

--- src/logger/logger_mod.f90
+++ src/logger/logger_mod.f90
@@ -3,14 +3,18 @@ module logger_mod
 
     use, intrinsic :: iso_fortran_env, only : output_unit, error_unit
 
+    use colour_mod, only: change_colour, COLOUR_WHITE, COLOUR_GREY
+
     implicit none
 
     private
 
+    !> Logger class
     type, public :: logger_type
         character(len=16) :: id = "None"
         integer           :: log_level
         integer           :: output_stream
+        integer           :: colour_tag = COLOUR_WHITE
     contains
         procedure, public :: event
         procedure         :: format_info
@@ -23,17 +27,19 @@ module logger_mod
 contains
 
     !> Constructor for the logger object
-    function logger_constructor(id) result(self)
+    function logger_constructor(id, colour) result(self)
 
         implicit none
 
         type(logger_type) :: self
         character(len=*), optional, intent(in) :: id
+        integer,          optional, intent(in) :: colour
 
         self%log_level = 100
         self%output_stream = output_unit
 
         if (present(id)) self%id = trim(id)
+        if (present(colour)) self%colour_tag = colour
 
     end function logger_constructor
 
@@ -49,14 +55,17 @@ contains
         character(len=*),   intent(in)    :: message
         integer,            intent(in)    :: level
 
-        character(len=25) :: dt_str
-        character(len=32) :: info_str
+        character(len=25)  :: dt_str
+        character(len=32)  :: info_str
+        character(len=128) :: colour_dt_str, colour_info_str
 
         dt_str = datetime_string()
         info_str = self%format_info()
 
-        write ( self%output_stream, '("[", A,"] | " A, ": ", A)' ) &
-            trim(dt_str), trim(info_str), trim(message)
+        colour_dt_str = change_colour(dt_str, COLOUR_GREY)
+
+        write ( self%output_stream, '("[", A,"]" A, ": ", A)' ) &
+            trim(colour_dt_str), trim(info_str), trim(message)
 
     end subroutine event
 
@@ -69,12 +78,13 @@ contains
 
         class(logger_type), intent(inout) :: self
 
-        character(len=32) :: result_string
+        character(len=32) :: result_string, colour_tag_str
 
         if (trim(self%id) == "None") then
-            write( result_string, '(A)' ) "TEST"
+            write( result_string, '(" ", A)' ) "TEST"
         else
-            write( result_string, '(A, " | ", A)' ) trim(self%id), "TEST"
+            colour_tag_str = change_colour(trim(self%id), self%colour_tag)
+            write( result_string, '("[", A,"] ", A)' ) trim(colour_tag_str), "TEST"
         end if
 
     end function format_info
--- src/oolong.f90
+++ src/oolong.f90
@@ -1,10 +1,14 @@
 module oolong
 
   use logger_mod, only: logger_type
+  use colour_mod, only: COLOUR_WHITE, COLOUR_GREY, COLOUR_RED, COLOUR_BLUE, &
+                        COLOUR_GREEN, COLOUR_YELLOW
 
   implicit none
   private
 
   public :: logger_type
-  
+  public :: COLOUR_WHITE, COLOUR_GREY, COLOUR_RED, COLOUR_BLUE, &
+            COLOUR_GREEN, COLOUR_YELLOW
+
 end module oolong
